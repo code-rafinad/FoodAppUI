f74cc3a578388322d7f5bd88cb3c217c8f0c3c40
Rafinad <rafinad@outlook.com>
2021-05-06 22:16:20 +0300

    Initial Commit

---
+++ FoodAppUI/FoodAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppUIApp.swift
+//  FoodAppUI
+//
+//  Created by Сахар Кубический on 06.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
